bca03047b8f31468792cce14d7b8f72f8925b662
byte4all <190314524+byte4all@users.noreply.github.com>
2026-09-07 11:34:54 +0800

    Changed to 60 RM

--- components/layout/Banner/TopBanner.tsx
+++ components/layout/Banner/TopBanner.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 const TopBanner = () => {
   return (
     <div className="bg-brand text-white text-center py-2 px-2 sm:px-4 xl:px-0">
       <div className="max-w-frame mx-auto">
         <p className="text-xs sm:text-sm">
-          Get Free Shipping Across Kuala Lumpur for orders above 35 RM.
+          Get Free Shipping Across Kuala Lumpur for orders above 60 RM.
         </p>
       </div>
     </div>
